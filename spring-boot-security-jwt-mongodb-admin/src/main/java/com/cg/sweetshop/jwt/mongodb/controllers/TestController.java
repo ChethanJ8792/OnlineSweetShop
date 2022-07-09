90060d26054ab8d5db559000af920f9ed06d6780
ChethanJ8792 <chethan2jsr4@gmail.com>
2022-07-09 14:22:17 +0530

    Added Jwt based on role admin only

---
+++ spring-boot-security-jwt-mongodb-admin/src/main/java/com/cg/sweetshop/jwt/mongodb/controllers/TestController.java
@@ -0,0 +1,31 @@
+package com.cg.sweetshop.jwt.mongodb.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+	@GetMapping("/all")
+	//@PreAuthorize("hasRole('UNDEFINED')")
+	public String allAccess() {
+		return "Public Content.";
+	}
+	
+	@GetMapping("/user")
+	@PreAuthorize("hasRole('ADMIN')")
+	public String userAccess() {
+		return "User Content.";
+	}
+
+	
+	@GetMapping("/admin")
+	@PreAuthorize("hasRole('ADMIN')")
+	public String adminAccess() {
+		return "Admin Board.";
+	}
+}
---
+++ spring-boot-security-jwt-mongodb-admin/src/main/java/com/cg/sweetshop/jwt/mongodb/models/ERole.java
@@ -0,0 +1,8 @@
+package com.cg.sweetshop.jwt.mongodb.models;
+
+public enum ERole {
+ //ROLE_USER,
+  //ROLE_MODERATOR,
+  ROLE_ADMIN,
+  ROLE_UNDEFINED
+}
